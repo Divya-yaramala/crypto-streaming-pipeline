a3160549f27c45f01b1c640199eb1b7397881862
Divya Yaramala <divyayaramala145@gmail.com>
2026-06-04 13:26:18 -0400

    Project 2 Day 3: add storage config module

---
+++ storage/config.py
@@ -0,0 +1,9 @@
+import os
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
+POSTGRES_USER = os.getenv("POSTGRES_USER", "crypto_user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "crypto_pass")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "crypto_db")
+
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
